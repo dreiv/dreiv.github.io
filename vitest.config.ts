7425e72a37a7271fde48a6b2088ad45de6da25bd
Voicu Andrei <dreivoicu@gmail.com>
2026-07-04 10:44:31 +0300

    chore: update scaffolding dependencies

---
+++ vitest.config.ts
@@ -0,0 +1,46 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import { playwright } from '@vitest/browser-playwright'
+import viteConfig from './vite.config'
+
+const commonTestConfig = {
+  setupFiles: [fileURLToPath(new URL('./src/test/setup.ts', import.meta.url))],
+  globals: true,
+}
+
+export default defineConfig({
+  test: {
+    ...commonTestConfig,
+    root: fileURLToPath(new URL('./', import.meta.url)),
+    projects: [
+      /* UNIT TESTS */
+      {
+        ...mergeConfig(viteConfig, {
+          test: {
+            ...commonTestConfig,
+            name: 'unit',
+            environment: 'node',
+            include: ['src/**/*.unit.{test,spec}.ts'],
+            exclude: [...configDefaults.exclude, 'e2e/**'],
+          },
+        }),
+      },
+      /* BROWSER (DOM/COMPONENT) TESTS */
+      {
+        ...mergeConfig(viteConfig, {
+          test: {
+            ...commonTestConfig,
+            name: 'browser',
+            include: ['src/**/*.browser.{test,spec}.ts'],
+            browser: {
+              enabled: true,
+              provider: playwright(),
+              instances: [{ browser: 'chromium' }],
+              headless: true,
+            },
+          },
+        }),
+      },
+    ],
+  },
+})
